07bd5bc2adcbc26daea3506351d4e487f78799b9
YuriiPohorilets <pohorilets.y@gmail.com>
2023-04-14 10:35:48 +0300

    setup gh-pages

--- src/pages/Tweets.jsx
+++ src/pages/Tweets.jsx
@@ -1,17 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { Box } from '@mui/material';
 import { getUsers } from 'utils/usersApi';
 import { TweetsList } from 'components/TweetsList/TweetsList';
 import { Pagination } from 'components/Pagination/Pagination';
 import { GoBackButton } from 'components/GoBackButton/GoBackButton';
 import { Filter } from 'components/Filter/Filter';
+import { useLocalStorage } from 'hooks/useLocalStorage';
+
+const LS_KEY = 'users';
 
 export const Tweets = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useLocalStorage('users', []);
+
+  if (users.length === 0) {
+    getUsers().then(setUsers);
+  }
 
   useEffect(() => {
-    getUsers().then(setUsers);
-  }, []);
+    localStorage.setItem(LS_KEY, JSON.stringify(users));
+  }, [users]);
 
   return (
     <Box
